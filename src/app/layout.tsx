b387c3cf4053f21341a8c62d9a310b8f16ead35a
Tyler <m0bley@icloud.com>
2023-11-22 10:12:53 +0000

    updated README and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className="{inter.className} flex flex-col min-h-screen font-mono min-w-screen text-neon-green max-w-2xl mx-auto">
+            <body className="{inter.className} flex flex-col min-h-screen font-mono min-w-screen text-neon-green max-w-3xl mx-auto">
                 <Header />
                 {children}
                 <Footer />
